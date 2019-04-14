38e8ac42ad14f350a1db4fec6f258fed309fc433
jacksonnadar <49579140+jacksonnadar@users.noreply.github.com>
2019-04-14 15:33:39 +0530

    website commit

---
+++ New folder (3)/js/cake.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+  $('#view').setZoomPicture({
+
+    thumbsContainer: '#pics-thumbs',
+
+    prevContainer: '#nav-left-thumbs',
+
+    nextContainer: '#nav-right-thumbs',
+
+    zoomContainer: '#zoom'
+
+  });
+
+});
+/*. Set the zoom level whatever you like.*/
+
+
+$(document).ready(function() {
+2
+  $('#view').setzoomPicture({
+
+    thumbsContainer: '#pics-thumbs',
+
+    prevContainer: '#nav-left-thumbs',
+
+    nextContainer: '#nav-right-thumbs',
+
+    zoomContainer: '#zoom',
+
+    zoomLevel: 2
+
+  });
+
+});
+/*. Customize the image loading message.*/
+
+/*view source*/
+
+$(document).ready(function() {
+
+  $('#view').setZoomPicture({
+
+    thumbsContainer: '#pics-thumbs',
+
+    prevContainer: '#nav-left-thumbs',
+
+    nextContainer: '#nav-right-thumbs',
+
+    zoomContainer: '#zoom',
+
+    loadMsg: 'Loading...'
+
+  });
+
+});
